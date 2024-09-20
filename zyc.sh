da7ab98d0654eba6b8152212b506e655a4878f8f
fredslbn <fh.silaban@gmail.com>
2024-09-20 19:35:39 +0700

    add zyc compiler

--- zyc.sh
+++ zyc.sh
@@ -42,7 +42,11 @@ ZIPNAME="SUPER.KERNEL.MOONSTONE-(clanglinaro)-$(TZ=Asia/Jakarta date +"%Y%m%d-%H
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=clang19-7
+COMPILER=zyc
+
+CrossCompileFlagTriple="aarch64-linux-gnu-"
+CrossCompileFlag64="aarch64-linux-gnu-"
+CrossCompileFlag32="arm-linux-gnueabi-"
 
 ##----------------------------------------------------------##
 # Specify Linker
@@ -54,28 +58,7 @@ LINKER=ld.lld
 # Clone ToolChain
 function cloneTC() {
               
-    if [ $COMPILER = "clang17-7" ];
-	then
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229b.tar.gz && mkdir clang && tar -xzf clang-r498229b.tar.gz -C clang/
-    export KERNEL_CLANG_PATH="${KERNEL_DIR}/clang"
-    export KERNEL_CLANG="clang"
-    export PATH="$KERNEL_CLANG_PATH/bin:$PATH"
-    CLANG_VERSION=$(clang --version | grep version | sed "s|clang version ||")
-	
-    wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz && tar -xf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
-    mv gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu gcc64
-    export KERNEL_CCOMPILE64_PATH="${KERNEL_DIR}/gcc64"
-    export KERNEL_CCOMPILE64="aarch64-linux-gnu-"
-    export PATH="$KERNEL_CCOMPILE64_PATH/bin:$PATH"
-    GCC_VERSION=$(aarch64-linux-gnu-gcc --version | grep "(GCC)" | sed 's|.*) ||')
-   
-    wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz && tar -xf gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
-    mv gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf gcc32
-    export KERNEL_CCOMPILE32_PATH="${KERNEL_DIR}/gcc32"
-    export KERNEL_CCOMPILE32="arm-linux-gnueabihf-"
-    export PATH="$KERNEL_CCOMPILE32_PATH/bin:$PATH"   
-    
-    elif [ $COMPILER = "clang19-7" ];
+    if [ $COMPILER = "clang19-7" ];
 	then
 	wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r530567.tar.gz && mkdir clang && tar -xzf clang-r530567.tar.gz -C clang/
     export KERNEL_CLANG_PATH="${KERNEL_DIR}/clang"
@@ -95,21 +78,20 @@ function cloneTC() {
     export KERNEL_CCOMPILE32_PATH="${KERNEL_DIR}/gcc32"
     export KERNEL_CCOMPILE32="arm-linux-gnueabihf-"
     export PATH="$KERNEL_CCOMPILE32_PATH/bin:$PATH"
-	
-    
+	  
     elif [ $COMPILER = "zyc" ];
       then   
       mkdir -p clang-zyc
       cd clang-zyc
       wget -q $(curl -k https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-link.txt 2>/dev/null) -O "zyc-clang.tar.gz"
       tar -xf zyc-clang.tar.gz
+      rm -f zyc-clang.tar.gz
+      cd ..
       export KERNEL_CLANG_PATH="${KERNEL_DIR}/clang-zyc"
       export KERNEL_CLANG="clang"
       export PATH="$KERNEL_CLANG_PATH/bin:$PATH"
       CLANG_VERSION=$(clang --version | grep version | sed "s|clang version ||")
-      rm -f zyc-clang.tar.gz
-      cd ..    
-    
+      
 	fi
 	
     # Clone AnyKernel
@@ -165,9 +147,9 @@ START=$(date +"%s")
 	       make -j$(nproc --all) O=out \
 	       ARCH=arm64 \
 	       CC=$KERNEL_CLANG \
-           CROSS_COMPILE=$KERNEL_CCOMPILE64 \
-           CROSS_COMPILE_ARM32=$KERNEL_CCOMPILE32 \
-           CLANG_TRIPLE=aarch64-linux-gnu- \
+           CLANG_TRIPLE=${CrossCompileFlagTriple} \
+           CROSS_COMPILE=${CrossCompileFlag64} \
+           CROSS_COMPILE_ARM32=${CrossCompileFlag32} \
            LD=${LINKER} \
            LLVM=1 \
            LLVM_IAS=1 \
@@ -183,9 +165,7 @@ START=$(date +"%s")
 	       
 	fi
 }
-CrossCompileFlagTriple="aarch64-linux-gnu-"
-CrossCompileFlag64="aarch64-linux-gnu-"
-CrossCompileFlag32="arm-linux-gnueabi-"
+
 ##----------------------------------------------------------------##
 function zipping() {
 	# Copy Files To AnyKernel3 Zip
